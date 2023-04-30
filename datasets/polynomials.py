e4236a17e7fd2657397dd19c9e987cc47aee0f86
Samuel Sanft <sanft@princeton.edu>
2023-04-30 02:43:49 -0400

    Add option for second order polynomials for higher dimensional inputs to Polynomial datasets

--- datasets/polynomials.py
+++ datasets/polynomials.py
@@ -16,45 +16,50 @@ class Polynomial:
             if self.dim == 1:
                 for i in range(self.order + 1):
                     mean = 0
-                    dev = 10 * 6 ** (-i)
+                    dev = 16 * 4 ** (-i)
                     self.coefficients.append(random.gauss(mean, dev))
             elif self.dim > 1 and self.order == 1:
+                mean = 0
+                self.coefficients.append(random.gauss(mean, 16))  # constant
                 for i in range(self.dim + 1):
-                    mean = 0
-                    dev = random.gauss(0, 1)
-                    self.coefficients.append(random.gauss(mean, dev))
+                    self.coefficients.append(random.gauss(mean, 4))  # first order terms
+            elif self.dim > 1 and self.order == 2:
+                self.coefficients.append(random.gauss(0, 16))  # constant
+                for i in range(self.dim):  # first order terms
+                    self.coefficients.append(random.gauss(0, 4))
+                for i in range(self.dim):  # second order terms
+                    for j in range(i, self.dim):
+                        self.coefficients.append(random.gauss(0, 1))
             else:
-                raise Exception("Illegal order/dimension combination (either order or dim must be 1).")
+                raise Exception("Illegal order/dimension combination (only first and second order polynomials allowed "
+                                "when input dimension is larger than 1).")
 
             self.coefficients = np.array(self.coefficients)
 
     def generate(self, num_points, x_dev=3, y_dev=1, bias=0):
-        x = []
-        y = []
+        x = np.empty((num_points, self.dim)) if self.dim > 1 else np.empty(num_points)
+        y = np.empty(num_points)
 
         if not isinstance(x_dev, (list, tuple, np.ndarray)):
             x_dev = [x_dev] * self.dim
 
         for i in range(num_points):
-            y_val = random.gauss(bias, y_dev)
-            if self.dim == 1:
-                x_val = random.gauss(0, x_dev[0])
-                for j in range(self.order + 1):
-                    y_val += self.coefficients[j] * x_val ** j
-                x.append(x_val)
-                y.append(y_val)
-            else:
-                x_vals = []
-                for j in range(self.dim):
-                    x_vals.append(random.gauss(0, x_dev[j]))
-                x_vals = np.array(x_vals)
-                y_val += self.coefficients[0]
-                y_val += x_vals @ self.coefficients[1:]
-                x.append(x_vals)
-                y.append(y_val)
+            x[i] = random.gauss(0, x_dev[0]) if self.dim == 1 \
+                else np.array([random.gauss(0, x_dev[j]) for j in range(self.dim)])
 
-        x = np.array(x)
-        y = np.array(y)
+            noise = random.gauss(bias, y_dev)
+            if self.dim == 1:
+                y[i] = noise + sum([self.coefficients[j] * x[i] ** j for j in range(len(self.coefficients))])
+            elif self.order == 1:
+                y[i] = noise + self.coefficients[0] + x[i] @ self.coefficients[1:]
+            elif self.order == 2:
+                y[i] = noise + self.coefficients[0] + x[i] @ self.coefficients[1:self.dim + 1]
+                index = self.dim + 1
+                for a in range(self.dim):
+                    for b in range(a, self.dim):
+                        y[i] += x[i][a] * self.coefficients[index]
+                        index += 1
+
         return x, y
 
     def __str__(self):
